38f0abe4a0138bb28e6b73f745b948b8eecc1dd5
Ralf Geschke <ralf@kuerbis.org>
2018-11-10 20:35:13 +0100

    Work in progress on default header elements, test some Go template functions & features

--- static/js/functions.js
+++ static/js/functions.js
@@ -156,6 +156,8 @@
         } else { // >= 1200
             mediaDetectSize = 'xl';
 		}
+		console.log(mediaSize);
+		console.log(mediaDetectSize);
         if (mediaDetectSize != mediaSize || mediaDetectSize == 'xs') {
             mediaSize = mediaDetectSize;
             $.headerImageResize(mediaSize);
@@ -172,51 +174,7 @@
     });
 
 
-	// Enable menu toggle for small screens.
-	/*( function() {
-		var nav = $( '#primary-navigation' ), button, menu;
-		if ( ! nav ) {
-			return;
-		}
-
-		button = nav.find( '.menu-toggle' );
-		if ( ! button ) {
-			return;
-		}
-
-		// Hide button if menu is missing or empty.
-		menu = nav.find( '.nav-menu' );
-		if ( ! menu || ! menu.children().length ) {
-			button.hide();
-			return;
-		}
-
-		$( '.menu-toggle' ).on( 'click.azbalac', function() {
-			nav.toggleClass( 'toggled-on' );
-		} );
-	} )();
-*/
-	/*
-	 * Makes "skip to content" link work correctly in IE9 and Chrome for better
-	 * accessibility.
-	 *
-	 * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
-	 */
-/*	_window.on( 'hashchange.azbalac', function() {
-		var element = document.getElementById( location.hash.substring( 1 ) );
-
-		if ( element ) {
-			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
-				element.tabIndex = -1;
-			}
-
-			element.focus();
-
-			// Repositions the window on jump-to-anchor to account for header height.
-			window.scrollBy( 0, -80 );
-		}
-	} );
-*/
+	
 	$( function() {
 		// Search toggle.
 		$( '.search-toggle' ).on( 'click.azbalac', function( event ) {
@@ -233,75 +191,17 @@
 
 
 
-
-
-		/*
-		 * Fixed header for large screen.
-		 * If the header becomes more than 48px tall, unfix the header.
-		 *
-		 * The callback on the scroll event is only added if there is a header
-		 * image and we are not on mobile.
-		 */
-/*		if ( _window.width() > 781 ) {
-			var mastheadHeight = $( '#masthead' ).height(),
-				toolbarOffset, mastheadOffset;
-
-			if ( mastheadHeight > 48 ) {
-				body.removeClass( 'masthead-fixed' );
-			}
-
-			if ( body.is( '.header-image' ) ) {
-				toolbarOffset  = body.is( '.admin-bar' ) ? $( '#wpadminbar' ).height() : 0;
-				mastheadOffset = $( '#masthead' ).offset().top - toolbarOffset;
-
-				_window.on( 'scroll.azbalac', function() {
-					if ( ( window.scrollY > mastheadOffset ) && ( mastheadHeight < 49 ) ) {
-						body.addClass( 'masthead-fixed' );
-					} else {
-						body.removeClass( 'masthead-fixed' );
-					}
-				} );
-			}
-		}
-*/
-		// Focus styles for menus.
-		/*$( '.primary-navigation, .secondary-navigation' ).find( 'a' ).on( 'focus.azbalac blur.azbalac', function() {
-			$( this ).parents().toggleClass( 'focus' );
-		} );
-        */
-
 	} );
-
-	// Arrange footer widgets vertically.
-	/*if ( $.isFunction( $.fn.masonry ) ) {
-		$( '#footer-sidebar' ).masonry( {
-			itemSelector: '.widget',
-			columnWidth: function( containerWidth ) {
-				return containerWidth / 4;
-			},
-			gutterWidth: 0,
-			isResizable: true,
-			isRTL: $( 'body' ).is( '.rtl' )
-		} );
-	}
-*/
-	// Initialize Featured Content slider.
-/*	_window.load( function() {
-		if ( body.is( '.slider' ) ) {
-			$( '.featured-content' ).featuredslider( {
-				selector: '.featured-content-inner > article',
-				controlsContainer: '.featured-content'
-			} );
-		}
-	} );
-	*/
 
     $(window).on('load', function() {
+		console.log(azbalacHeaderImage);
         if (!$('#site-header-image').length) {
             return;
         }
         $.checkMediaSize();
-    });
+	});
+
+	
 
 $( ".innersocial" ).hover(
   function() {
@@ -338,6 +238,26 @@ $( '.dropdown-menu a.dropdown-toggle' ).on( 'click', function ( e ) {
 
 	return false;
 } );
+
+
+$(window).on('load', function() {
+var foobar = $('#main').width();
+console.log(foobar);
+var elementSize = foobar;
+if (elementSize < 540) {
+	mediaDetectSize = 'xs';
+} else if (elementSize >= 540 && elementSize < 720) { 
+	mediaDetectSize = 'sm';
+} else if (elementSize >= 720 && elementSize < 960) { 
+	mediaDetectSize = 'md';
+} else if (elementSize >= 960 && elementSize < 1140) { 
+	mediaDetectSize = 'lg';
+} else { // >= 1200
+	mediaDetectSize = 'xl';
+}
+console.log(mediaSize);
+console.log(mediaDetectSize);
+});
 
  
 } )( jQuery );
